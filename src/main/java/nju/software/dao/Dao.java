6994b283b5cc1be0caad3bdbd150843e1378c43a
lab <furnace09@gmail.com>
2016-01-12 20:37:24 +0800

    some basic configuration

--- src/main/java/nju/software/dao/Dao.java
+++ src/main/java/nju/software/dao/Dao.java
@@ -4,4 +4,5 @@ package nju.software.dao;
  * Created by lab on 16-1-12.
  */
 public class Dao {
+
 }
---
+++ src/main/java/nju/software/util/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Provide some basic util for implementation
+ * Created by lab on 16-1-12.
+ */
+package nju.software.util;
