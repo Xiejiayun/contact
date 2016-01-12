fd7355561bda884c0339665c10f232bab88ea66a
lab <furnace09@gmail.com>
2016-01-12 21:29:30 +0800

    some basic configuration connect to database

--- src/main/java/nju/software/dao/Dao.java
+++ src/main/java/nju/software/dao/Dao.java
@@ -1,8 +1,45 @@
 package nju.software.dao;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by lab on 16-1-12.
  */
 public class Dao {
 
+    private static Logger logger = LogManager.getLogger(Dao.class);
+    public static void main(String[] args) {
+        logger.info("start connect to the database");
+        new Dao().connectDB();
+        logger.info("finish connect to the database");
+    }
+
+    public void connectDB() {
+        String driverName = "com.mysql.jdbc.Driver";
+        try {
+            Class.forName(driverName); // here is the ClassNotFoundException
+            String serverName = "localhost";
+            String database = "contact";
+            String url = "jdbc:mysql://" + serverName + "/" + database;
+
+            String username = "root";
+            String password = "xiexie";
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.printf("Success");
+            logger.info("success connect to the database");
+        } catch (ClassNotFoundException e) {
+            logger.error("Error occurs", e);
+            e.printStackTrace();
+        } catch (SQLException e) {
+            logger.error("Error occurs", e);
+            e.printStackTrace();
+        }
+
+
+    }
 }
